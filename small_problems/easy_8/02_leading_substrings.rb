87a71bffc283f1368c4be460378df5b16d8b32d4
Robyn Tang <tang.robyn@gmail.com>
2023-02-21 02:11:25 +0000

    2/20/23

--- small_problems/easy_8/02_leading_substrings.rb
+++ small_problems/easy_8/02_leading_substrings.rb
@@ -1,2 +1,31 @@
-"# 02_leading_substrings.rb"
+# 02_leading_substrings.rb
 
+# Write a method that returns a list of all substrings
+# of a string that start at the beginning of the original string.
+# The return value should be arranged in order from shortest to
+# longest substring.
+
+
+# def leading_substrings(string)
+#   array = string.split('')
+#   new_array = []
+#   counter = 0
+#   loop do
+#     new_array << array[0..counter].join
+#     counter += 1
+#     break if counter == array.size
+#   end
+#   new_array
+# end
+
+def leading_substrings(string)
+  result = []
+  0.upto(string.size - 1) do |index|
+    result << string[0..index]
+  end
+  result
+end
+
+p leading_substrings('abc') == ['a', 'ab', 'abc']
+p leading_substrings('a') == ['a']
+p leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
--- small_problems/easy_8/03_all_substrings.rb
+++ small_problems/easy_8/03_all_substrings.rb
@@ -1,1 +1,23 @@
 # 03_all_substrings.rb
+
+def leading_substrings(string)
+  result = []
+  0.upto(string.size - 1) do |index|
+    result << string[0..index]
+  end
+  result
+end
+
+def substrings(string)
+  result = []
+  counter = 0
+
+    loop do
+      result << leading_substrings(string[counter..-1])
+      counter += 1
+      break if counter == string.size
+    end
+    result.flatten
+end 
+
+p substrings('abcde')
