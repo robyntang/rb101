8190cd46fc6b2a40610bb1dd6ad3e68ed841e716
Robyn Tang <tang.robyn@gmail.com>
2023-02-27 22:57:00 +0000

    2/27/23

--- small_problems/easy_9/08_grocery_list.rb
+++ small_problems/easy_9/08_grocery_list.rb
@@ -1,4 +1,27 @@
 #08_grocery_list.rb
 
-def buy_fruit(array1, array2, array3)
+# def buy_fruit(((array1, number),(array2, number2),(array3, number3)))
+#   new_array = []
+#   number.times { new_array << array1}
+#   number2.times { new_array << array2}
+#   number3.times { new_array << array3}
+#   new_array
+# end
+
+# def buy_fruit(list)
+#   expanded_list = []
+
+#   list.each do |item|
+#     fruit, quantity = item[0], item[1]
+#     quantity.times { expanded_list << fruit}
+#   end
+
+#   expanded_list
+# end
+
+def buy_fruit(list)
+  list.map { |fruit, quantity| [fruit] * quantity }.flatten
 end
+
+
+p buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]])
