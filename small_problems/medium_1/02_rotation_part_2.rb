dd7653bda59c86b1b4449e1dfd9c671ac0643d79
Robyn Tang <tang.robyn@gmail.com>
2023-03-04 02:52:01 +0000

    3/3/2023

--- small_problems/medium_1/02_rotation_part_2.rb
+++ small_problems/medium_1/02_rotation_part_2.rb
@@ -5,8 +5,23 @@ def rotate_array(array)
 end
 
 
-
-
 def rotate_rightmost_digits(number, idx)
-
+  array = number.to_s.split('')
+  array[-idx..-1] = rotate_array(array[-idx..-1])
+  array.join.to_i
 end
+
+
+# def rotate_rightmost_digits(number, n)
+#   all_digits = number.to_s.chars
+#   all_digits[-n..-1] = rotate_array(all_digits[-n..-1])
+#   all_digits.join.to_i
+# end
+
+
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
+p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
