3352b68587871c953d834c987a21f5262de2dd30
Robyn Tang <tang.robyn@gmail.com>
2023-03-10 01:59:22 +0000

    3/9/23

--- small_problems/medium_1/05_diamonds.rb
+++ small_problems/medium_1/05_diamonds.rb
@@ -1,4 +1,25 @@
 # 05_diamonds.rb
 
-def diamond(integer)
+# def triangle(num)
+#   (num + 1).times { |i| puts ( " " * (num - i)) + ("*" * i) }
+# end
+
+# triangle(3)
+
+def diamond(num)
+
+  1.upto(num).each do |digit|
+    unless digit.even?
+      puts " " * ((num - digit) / 2 ) + "*" * digit
+    end
+  end
+
+  (num - 1).downto(1).each do |digit|
+    unless digit.even?
+      puts " " * ((num - digit) / 2 ) + "*" * digit
+    end
+  end
+
 end
+
+diamond(13)
