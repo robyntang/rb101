55d574c978b2321f26430d159dfe532c45c45e13
Robyn Tang <tang.robyn@gmail.com>
2023-03-11 02:23:18 +0000

    3/10/2023

--- small_problems/medium_1/06_stack_machine_intepretation.rb
+++ small_problems/medium_1/06_stack_machine_intepretation.rb
@@ -1,1 +1,33 @@
 # 06_stack_machine_intepretation.rb
+
+
+def minilang(program)
+  register = 0
+  stack = []
+
+
+  program.split.each do |element|
+    case element 
+    when "PUSH"   then stack.push(register)
+    when "ADD"    then register += stack.pop
+    when "SUB"    then register -= stack.pop
+    when "MULT"   then register *= stack.pop
+    when "DIV"    then register /= stack.pop
+    when "MOD"    then register %= stack.pop
+    when "POP"    then register = stack.pop
+    when "PRINT"  then puts register
+    else register = element.to_i
+    end
+  end
+end
+
+minilang('PRINT')
+minilang('5 PUSH 3 MULT PRINT')
+minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+minilang('5 PUSH POP PRINT')
+minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+minilang('-3 PUSH 5 SUB PRINT')
+minilang('6 PUSH')
+
--- small_problems/medium_1/07_word_to_digit.rb
+++ small_problems/medium_1/07_word_to_digit.rb
@@ -1,1 +1,53 @@
 # 07_word_to_digit.rb
+
+# NUMBERS = {
+#   "zero" => 0,
+#   "one" => 1,
+#   "two" => 2,
+#   "three" => 3,
+#   "four" => 4,
+#   "five" => 5,
+#   "six" => 6,
+#   "seven" => 7,
+#   "eight" => 8,
+#   "nine" => 9,
+# }
+
+# def word_to_digit(string)
+# string.split.map! { |word| NUMBERS[word] || word }.join(' ')
+# end
+
+#the above example replaces the key with the value in the hash, but only if there is an exact correspondence.
+
+
+
+
+DIGIT_HASH = {
+  'zero' => '0', 'one' => '1', 'two' => '2', 'three' => '3', 'four' => '4',
+  'five' => '5', 'six' => '6', 'seven' => '7', 'eight' => '8', 'nine' => '9'
+}
+
+def word_to_digit(words)
+  DIGIT_HASH.keys.each do |word|
+    words.gsub!(/\b#{word}\b/, DIGIT_HASH[word])
+  end
+  words
+end
+
+
+# def word_to_digit(string)
+#   string.gsub!(/zero/,'0')
+#   string.gsub!(/one/,'1')
+#   string.gsub!(/two/,'2')
+#   string.gsub!(/three/,'3')
+#   string.gsub!(/four/,'4')
+#   string.gsub!(/five/,'5')
+#   string.gsub!(/six/,'6')
+#   string.gsub!(/seven/,'7')
+#   string.gsub!(/eight/,'8')
+#   string.gsub!(/nine/,'9')
+#   string
+# end
+
+
+p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
