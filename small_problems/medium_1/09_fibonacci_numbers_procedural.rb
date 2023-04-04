beffb0bd2b6a3f1a9955cae8488442a62ee4ee4f
Robyn Tang <tang.robyn@gmail.com>
2023-04-04 01:06:44 +0000

    4/3/2023

--- small_problems/medium_1/09_fibonacci_numbers_procedural.rb
+++ small_problems/medium_1/09_fibonacci_numbers_procedural.rb
@@ -1,1 +1,29 @@
 # 09_fibonacci_numbers_procedural.rb
+
+
+# def fibonacci(n)
+#   return 1 if n <= 2
+
+#   sequence = [1, 1]
+
+#   until sequence.length == n
+#   sequence << sequence[-1] + sequence[-2]
+#   end
+
+#   sequence[-1]
+# end
+
+def fibonacci(nth)
+  first, last = [1, 1]
+  3.upto(nth) do 
+    first, last = [last, first + last]
+  end
+
+  last
+end
+
+
+
+p fibonacci(20) == 6765
+p fibonacci(100) == 354224848179261915075
+fibonacci(100_001) # => 4202692702.....8285979669707537501
--- small_problems/medium_1/10_fibonacci_numbers_last_digit.rb
+++ small_problems/medium_1/10_fibonacci_numbers_last_digit.rb
@@ -1,1 +1,31 @@
 # 10_fibonacci_numbers_last_digit.rb
+
+# def fibonacci_last(nth)
+#   first, last = [1, 1]
+#   3.upto(nth) do 
+#     first, last = [last, first + last]
+#   end
+
+#   last.digits[0]
+# end
+
+def fibonacci_last(nth)
+  fibonacci(nth).to_s[-1].to_i
+end
+
+def fibonacci_last(nth)
+  last_2 = [1, 1]
+  3.upto(nth) do
+    last_2 = [last_2.last, (last_2.first + last_2.last) % 10]
+  end
+
+  last_2.last
+end
+
+
+p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+p fibonacci_last(123456789) # -> 4
